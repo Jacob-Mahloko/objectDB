4b6c80f3dc3a6fd263b6301cdc2f8ff22db3a682
Jacob-Mahloko <114677637+Jacob-Mahloko@users.noreply.github.com>
2023-08-22 16:47:47 +0200

    changed project name

--- database/src/P2/Trip.java
+++ database/src/P2/Trip.java
@@ -7,7 +7,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package practical2;
+package database;
 
 
 import java.io.Serializable;
